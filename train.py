1a2aa178d9aafc688e330bcb6a2b7e86e4099722
erwangccc <wangsc_up@163.com>
2020-09-02 13:34:43 +0800

    add graph to logs and make scheduler for lr

--- train.py
+++ train.py
@@ -1,16 +1,19 @@
 import argparse
+import torchvision
 from dataset import *
 import torch.optim as optim
+from torch.autograd import Variable
 from torch.utils.tensorboard import SummaryWriter
-from network import *
-# from resnet import ResNet, BasicBlock
+# from network import *
+from resnet import ResNet, BasicBlock
+from torch.optim import lr_scheduler
 from tqdm import tqdm
 import torch.nn as nn
 
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--epoches", type=int, default = 100, help= "number of epochs")
+    parser.add_argument("--epoches", type=int, default = 1, help= "number of epochs")
     parser.add_argument("--num_classes", type=int, default = 4, help= "number of classified classes")
     parser.add_argument("--input_size", type=int, default = 224, help= "input size of network")
     parser.add_argument("--batch_size", type=int, default = 32, help= "training batch sizes")
@@ -18,7 +21,7 @@ if __name__=='__main__':
     parser.add_argument("--n_cpu", type=int, default = 8, help= "cpu number used when training")
     parser.add_argument("--checkpoint_interval", type=int, default = 10, help= "interval between saving model weights")
     parser.add_argument("--pretrained_weights", type=str, default = None, help= "path of pretrained weights")
-    parser.add_argument("--log_path", type=str, default= "logs", help= "path of saving logs")
+    parser.add_argument("--log_path", type=str, default= "logs/resnet_classifier", help= "path of saving logs")
     parser.add_argument("--weights_path", type=str, default= "./", help= "path of saving weights")
     parser.add_argument("--train_path", type=str, default = "E://dataset//data//", help= "cpu number used when training")
 
@@ -26,25 +29,37 @@ if __name__=='__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     writer = SummaryWriter(args.log_path, 'Training process of cars detection via ResNet18!')
     CEloss = nn.CrossEntropyLoss()
-    # resnet = ResNet(BasicBlock, [2, 2, 2, 2], 4)
-    resnet = ResNet18(Basicblock, [2, 2, 2, 2], 224, 3, 4)
-    resnet = resnet.to(device)
+    resnet = ResNet(BasicBlock, [2, 2, 2, 2], 4)
+    # resnet = ResNet18(Basicblock, [2, 2, 2, 2], 224, 3, 4)
     resnet.apply(weights_init)
     optimizer = optim.SGD(resnet.parameters(), lr=args.lr, weight_decay = 0.9999)
+    scheduler = lr_scheduler.StepLR(optimizer, step_size=60, gamma=0.5)
     # load train dataset
     train_dataset = data_loader(args.train_path, args.input_size,
                                 args.batch_size, args.n_cpu)
 
     # write model structure to logs
-    # dummy_input = torch.randn(1, 3, 224, 224, requires_grad = True)
-    # grid = torchvision.utils.make_grid(dummy_input)
-    # writer.add_graph(resnet, grid)
+    # get some random training images
+    dataiter = iter(train_dataset)
+    _, images, labels = dataiter.next()
+
+    # create grid of images
+    img_grid = torchvision.utils.make_grid(images)
+
+    # write to tensorboard
+    writer.add_image('test', img_grid)
+    with writer as w:
+        w.add_graph(resnet, images)
+
+    resnet = resnet.to(device)
+
     for epoch in range(1, args.epoches + 1):
         resnet.train()
         train_loss = 0
         progressbar = tqdm(train_dataset)
         for batch_idx, (path, imgs, targets) in enumerate(progressbar):
             imgs = imgs.to(device)
+            targets = targets.to(device)
             # optimizer set to zero
             optimizer.zero_grad()
             output = resnet(imgs)
@@ -58,6 +73,7 @@ if __name__=='__main__':
                 epoch, batch_idx * len(imgs), len(train_dataset.dataset),
                        100. * (batch_idx+1) / len(train_dataset),
                        train_loss / (batch_idx+1) * len(imgs)))
+        scheduler.step()
         writer.add_scalar('loss/train', train_loss/len(train_dataset.dataset), epoch)
 
         if epoch%args.checkpoint_interval == 0:
